cbaa9a4804cfdf7d5abf08be32c4a0ab2f7e3c0c
Joseph Wangemann <fractalscape13@gmail.com>
2020-01-27 21:51:16 -0800

    fix currentId counter

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 //user logic
+var newBook = new AddressBook();
+
+// function displayContactDetails(addressBookToDisplay) {
+//     var contactsList = $("ul#contacts");
+//     var htmlForContactInfo = "";
+//     addressBookToDisplay.contacts.forEach(function(contact){
+//         htmlForContactInfo += "li id=" + contact.id + 
+//     });
+// }
+
 $(document).ready(function() {
     $("form#contactForm").submit(function() {
         event.preventDefault();
-        var newBook = new AddressBook();
         var userFirst = $("input#firstName").val();
         var userLast = $("input#lastName").val();
         var userNumber = $("input#phoneNumber").val();
         var newContact = new Contact(userFirst, userLast, userNumber);
-        newBook.contacts.push(newContact);
+        newBook.addContact(newContact);
         console.log(newBook);
-        $("#list").append(newContact.firstName + " " + newContact.lastName + " " + newContact.phoneNumber + "<br>");
+        console.log(newBook.currentId);
+        $("#contacts").append(newContact.firstName + " " + newContact.lastName + " " + newContact.phoneNumber + "<br>");
         $("form")[0].reset();
     });
 });
@@ -20,15 +30,41 @@ $(document).ready(function() {
 //business logic
 function AddressBook() {
     this.contacts = [];
+    this.currentId = 0
 }
 
-AddressBook.prototype.addContact = function() {
+AddressBook.prototype.addContact = function(contact) {
+    contact.id = this.assignId();
     this.contacts.push(contact);
 }
 
 AddressBook.prototype.assignId = function() {
-
+    this.currentId += 1;
+    return this.currentId;
 }
+
+AddressBook.prototype.findContact = function(id) {
+    for (var i=0; i< this.contacts.length; i++) {
+      if (this.contacts[i]) {
+        if (this.contacts[i].id == id) {
+          return this.contacts[i];
+        }
+      }
+    };
+    return false;
+  }
+  
+  AddressBook.prototype.deleteContact = function(id) {
+    for (var i=0; i< this.contacts.length; i++) {
+      if (this.contacts[i]) {
+        if (this.contacts[i].id == id) {
+          delete this.contacts[i];
+          return true;
+        }
+      }
+    };
+    return false;
+  }
 
 function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName,
